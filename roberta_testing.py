94e785ba70d721906c5a429b4a946edeb2839142
Paweł Gryka <pawel.m.gryka@gmail.com>
2025-02-25 23:07:37 +0100

    Update RoBERTa evaluation scripts

--- roberta_testing.py
+++ roberta_testing.py
@@ -1,29 +1,11 @@
-from typing import List
-import sys
-from dao.attribute import DAOAttributePL
-from models.attribute import AttributePLInDB
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from transformers_interpret import SequenceClassificationExplainer
 
 if __name__ == "__main__":
-    try:
-        text_id = str(sys.argv[1])
-        if len(text_id) < 5:
-            print("ObjectID too short")
-            exit(1)
-    except IndexError:
-        print("ObjectID was not provided")
-        exit(1)
-
-    dao_attribute: DAOAttributePL = DAOAttributePL(collection_name="attributes-24-12-16-recalc-24-12-22.1-pgryka")
-    selected_attribute: AttributePLInDB = dao_attribute.find_by_id(text_id)
-    if selected_attribute is None:
-        print(f"No object found with {text_id} ID")
-        exit(1)
-    text_to_check = selected_attribute.stylometrix_metrics.text
+    text_to_check = "Testowany tekst, bardzo długi i skomplikowany"
     model_name = "sdadas/polish-roberta-large-v2"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForMaskedLM.from_pretrained("./final_results")
+    model = AutoModelForMaskedLM.from_pretrained("./SCEIZKA_DO_WYTRENOWANEGO_MODELU")
 
     # Create the explainer
     cls_explainer = SequenceClassificationExplainer(
@@ -31,6 +13,7 @@ if __name__ == "__main__":
         tokenizer=tokenizer
     )
 
+    # In case of long texts, sentence explainer cannot process them at once
     encoding = tokenizer(
         text_to_check,
         max_length=512,
@@ -47,11 +30,11 @@ if __name__ == "__main__":
     # Process each chunk with the explainer
     all_chunk_attributions = []
     for i in range(n_chunks):
-        # Instead of using tokenizer.decode(..., skip_special_tokens=True),
-        # we reconstruct the text from tokens so that the exact tokens (including special tokens)
-        # are preserved.
+        # Previous attempt
+        # chunk_text = tokenizer.decode(encoding["input_ids"][i], skip_special_tokens=False)
+        #reencoded = tokenizer(chunk_text, max_length=514, truncation=True, padding="max_length", return_tensors="pt")
+
         tokens = tokenizer.convert_ids_to_tokens(encoding["input_ids"][i])
-        # Rebuild the string in a way that will yield the same tokens when re-tokenized.
         chunk_text = tokenizer.convert_tokens_to_string(tokens)
 
         print(f"\n--- Attributions for Chunk {i + 1} ---")
